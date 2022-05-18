557ce697870c44e27c2b5d4135944ca2b408e46e
sagar aggarwal <sagar_aggarwal@outlook.com>
2022-05-18 22:06:44 +0530

    Js based simple Calculator

---
+++ Calcuator/index.js
@@ -0,0 +1,25 @@
+function append(v){
+
+ 
+   var pre=document.getElementById("display").value;
+   if(pre!="Invalid"){
+      pre=pre+v;
+      document.getElementById("display").value=pre;
+   }
+   else{
+      document.getElementById("display").value=v;
+   }
+   
+}
+function calculate(){
+   try {
+      document.getElementById("display").value= eval(document.getElementById("display").value);
+      
+   } catch (error) {
+      document.getElementById("display").value="Invalid";
+   }
+  
+}
+function clr(){
+   document.getElementById("display").value="";
+}
